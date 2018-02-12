9808bbb6d3b5c61ae677849f446e4e4ddf50db71
zhangyanyan <zhangyanyan3@jd.com>
2018-02-12 11:09:21 +0800

    增加目录文文件

---
+++ src/page/App.vue
@@ -0,0 +1,39 @@
+<template>
+    <section class="app_wrap">
+        <transition name="fade" mode="out-in">
+            <router-view></router-view>
+        </transition>
+    </section>
+</template>
+<script>
+    export default {
+        name: 'app',
+        data(){
+            return{
+
+            }
+        },
+        created(){
+            if (/Android/gi.test(navigator.userAgent)) {
+                window.addEventListener('resize', function () {
+                    if (document.activeElement.tagName == 'INPUT' || document.activeElement.tagName == 'TEXTAREA') {
+                        window.setTimeout(function () {
+                            document.activeElement.scrollIntoViewIfNeeded();
+                        }, 0);
+                    }
+                })
+            }
+        }
+    }
+</script>
+<style lang="less" scoped>
+    .app_wrap{
+        height: 100%;
+    }
+    .fade-enter-active, .fade-leave-active {
+        transition: opacity .1s;
+    }
+    .fade-enter, .fade-leave-active {
+        opacity: 0;
+    }
+</style>
---
+++ webpack.base.config.js
@@ -0,0 +1,7 @@
+const webpack = require('webpack');
+var path = require('path');
+
+module.exports = {
+    entry: __dirname + "src/entry/main.js",
+
+};
